8856c5ae892793b59ead0910cf7cddc238cbc5c3
Ryan O'Connor <charle692@gmail.com>
2016-04-01 01:19:24 -0400

    added positive negative and neutral words as features

--- src/extractor/Extractor.java
+++ src/extractor/Extractor.java
@@ -3,6 +3,7 @@ package extractor;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map.Entry;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.BufferedReader;
@@ -13,14 +14,20 @@ public class Extractor {
 	
 	private static String input_text_file = "semeval_twitter_data.txt";
 	private static String output_text_file = "output.arff";
+	private static String pos_neg_text_file = "pos_neg.txt";
 	private static Stop_Word stopWord = new Stop_Word();
 	private static ArrayList<Tweet> tweetList = new ArrayList<Tweet>();
-	
+	private static HashMap<String, String> positiveArray = new HashMap<String, String>(2721);
+	private static HashMap<String, String> negativeArray = new HashMap<String, String>(4913);
+	private static HashMap<String, String> neutralArray = new HashMap<String, String>(571);
+
 	public static void main(String args[]) {
-		retreiveTweet();
-		outputTweetInArffFormat();
+		retrievePositiveAndNegativeWords();
+//		retreiveTweet();
+//		outputTweetInArffFormat();
+		outputPositiveAndNegativeWords();
 	}
-	
+
 	public static void retreiveTweet() {
 		
 		System.out.println("Start Retreive Tweet");
@@ -28,9 +35,10 @@ public class Extractor {
 		try (BufferedReader br = new BufferedReader(new FileReader(input_text_file)))
 		{
 			try {
-				
 				String sCurrentLine;
+
 				while ((sCurrentLine = br.readLine()) != null) {
+					int[] numberOfCategories = {0,0,0};
 					
 					String opinion = sCurrentLine.split("\t", -1)[2];
 					String message = sCurrentLine.split("\t", -1)[3];
@@ -56,10 +64,10 @@ public class Extractor {
 					}
 					
 					filteredMessage = filteredMessage.trim();
+					numberOfCategories = getNumberOfCategories(filteredMessage);
+					Tweet tweet = new Tweet(filteredMessage, opinion, numberOfCategories[0], numberOfCategories[1], numberOfCategories[2]);
 					
-					Tweet tweet = new Tweet(filteredMessage, opinion);
 					tweetList.add(tweet);
-					
 				}
 				
 			} finally {
@@ -89,12 +97,21 @@ public class Extractor {
 			out.write("@attribute category {positive,negative,neutral,objective}");
 			out.newLine();
 			
+			out.write("@attribute numberOfPositiveWords integer");
+			out.newLine();
+			
+			out.write("@attribute numberOfNegativeWords integer");
+			out.newLine();
+			
+			out.write("@attribute numberOfNeutralWords integer");
+			out.newLine();
+			
 			out.write("@data");
 			out.newLine();
 			
 			for (int i = 0; i < tweetList.size(); i++) {
 				Tweet tweet = tweetList.get(i);
-				out.write("\' " + tweet.getMessage() + " \'" + "," + tweet.getOpinion());
+				out.write("\' " + tweet.getMessage() + " \'" + "," + tweet.getOpinion() + "," + tweet.getPositiveNumber() + "," + tweet.getNegativeNumber() + "," + tweet.getNeutralNumber());
 				out.newLine();
 			}
 			
@@ -107,25 +124,102 @@ public class Extractor {
 		System.out.println("Complete Output Tweet");
 	}
 	
-	/**
-	 * Remove URL links from string
-	 * 
-	 * @param s
-	 * @return
-	 */
-	public static String removeURLFromString(String string) {
+public static void outputPositiveAndNegativeWords() {
+		System.out.println("Start Output words");
+		
+		try {
 
+			BufferedWriter out = new BufferedWriter(new FileWriter("pos_neg2.txt", false));
+			
+			for(Entry<String, String> entry : positiveArray.entrySet()) {
+				System.out.println("Running");
+			    String key = entry.getKey();
+			    out.write(key + " " + "positive");
+			    out.newLine();
+			}
+			
+			for(Entry<String, String> entry : negativeArray.entrySet()) {
+			    String key = entry.getKey();
+			    out.write(key + " " + "negative");
+			    out.newLine();
+			}
+			
+			for(Entry<String, String> entry : neutralArray.entrySet()) {
+			    String key = entry.getKey();
+			    out.write(key + " " + "neutral");
+			    out.newLine();
+			}
+			
+			out.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+	    }
+		
+		System.out.println("Complete Output words");
+	}
+	
+	public static void retrievePositiveAndNegativeWords() {
+		System.out.println("Start Retreive Pos Neg and Neutral Words");
+
+		try (BufferedReader br = new BufferedReader(new FileReader(pos_neg_text_file)))
+		{
+			try {
+				String sCurrentLine;
+				while ((sCurrentLine = br.readLine()) != null) {
+					String[] attributes = sCurrentLine.split(" ");
+					String word = attributes[2].replaceAll("word1=", "");
+					String priorpolarity = attributes[5].replaceAll("priorpolarity=", "");
+					
+					if (priorpolarity.equals("negative")) {
+						negativeArray.put(word, word);
+					} else if (priorpolarity.equals("positive")){
+						positiveArray.put(word, word);
+					} else if (priorpolarity.equals("neutral")) {
+						neutralArray.put(word, word);
+					} else {
+						//Only get's here when the priorpolarity is both or extra attributes are included in the line
+						//This is only about 10 of the lines. 
+					}
+				}
+				
+			} finally {
+				br.close();
+		    }
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("Complete Retreive Pos Neg and Neutral Words");
+	}
+	
+	public static int[] getNumberOfCategories(String message) {
+		int[] numberOfCategory = new int[3];
+		numberOfCategory[0] = 0;
+		numberOfCategory[1] = 0;
+		numberOfCategory[2] = 0;
+
+		String[] words = message.split(" ");
+
+		for (String word : words) {
+			if (positiveArray.get(word) != null) {
+				numberOfCategory[0]++;
+			} else if (negativeArray.get(word) != null) {
+				numberOfCategory[1]++;
+			} else if (neutralArray.get(word) != null) {
+				numberOfCategory[2]++;
+			}
+		}
+
+		return numberOfCategory;
+	}
+
+	public static String removeURLFromString(String string) {
         string = string.replaceAll("https?://\\S+\\s?", ""); //Removes URL that begin with http
         string = string.replaceAll("www.\\S+\\s?", ""); //Removes URL that being with www
 		return string;
 	}
 	
-	/**
-	 * Remove non alphabets from string.
-	 * 
-	 * @param string
-	 * @return
-	 */
 	public static String removeNonAlphabetFromString(String string) {
 		string = string.replaceAll("[^A-Za-z ]", "");
 		return string;
@@ -135,8 +229,4 @@ public class Extractor {
 		string = string.replaceAll("'", "");
 		return string;
 	}
-
-	/**
-	 * Adds filtered tweets to document containing filtered tweets.
-	 */
 }
--- src/extractor/Tweet.java
+++ src/extractor/Tweet.java
@@ -3,10 +3,16 @@ package extractor;
 public class Tweet {
 	private String message;
 	private String opinion;
+	private int positiveNumber;
+	private int negativeNumber;
+	private int neutralNumber;
 	
-	public Tweet(String m, String o) {
+	public Tweet(String m, String o, int positiveNumber, int negativeNumber, int neutralNumber) {
 		message = m;
 		opinion = o;
+		this.neutralNumber = neutralNumber;
+		this.positiveNumber = positiveNumber;
+		this.negativeNumber = negativeNumber;
 	}
 	
 	public String getMessage() {
@@ -15,5 +21,17 @@ public class Tweet {
 	
 	public String getOpinion() {
 		return opinion;
+	}
+	
+	public int getPositiveNumber() {
+		return positiveNumber;
+	}
+	
+	public int getNegativeNumber() {
+		return negativeNumber;
+	}
+	
+	public int getNeutralNumber() {
+		return neutralNumber;
 	}
 }
